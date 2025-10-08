49d42964334b4c9c87aa37e8cad0776ecef51301
jfks18 <jfksuyat@gmail.com>
2025-10-08 18:37:00 +0800

    Force dynamic rendering for /registration/print to avoid prerender errors

--- src/app/registration/print/page.tsx
+++ src/app/registration/print/page.tsx
@@ -1,10 +1,13 @@
-"use client"
+// Force this route to be rendered dynamically at request time instead of prerendered
+export const dynamic = "force-dynamic";
+
 import React from 'react'
 import Print from '../components/print'
 
 export default function PrintPage() {
   return (
     <div>
+      {/* Print is a client component (uses window.print) */}
       <Print />
     </div>
   )
